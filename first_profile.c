330cb1bda2fa55826f62d2793dc63b9a48db2c55
anishkarise <ranishk38@gmail.com>
2020-07-07 23:52:34 +0530

    updating scanf

--- first_profile.c
+++ first_profile.c
@@ -1,43 +1,54 @@
 #include<stdio.h>
 void main ()
-{int age ;
-char name[10];
-int choice;
-scanf("%s",name);
+{int age,choice,salary,school;
+char company_name[20],y_n,school_name[30],hospital_name,youtube_channel_name,what_job_name;
+//scanf("%[^\n]%*c");
 printf("hello!");
 printf("tell me your age?\n");
 scanf("%d",&age);
 printf("your age is %d\n",age);
 printf("\n");
-if(age<13)
+if(age<21)
     {
         printf("you are kid can't go ahead\n");
+        getch();
     }
 else
     {
         printf("you are big kid you can continue\n\n");
-    }
-printf("what work do you do?\n");
+        printf("what work do you do?\n");
+        printf("1. engineer \n 2. doctor \n 3. teacher \n 4. youtuber \n 5. other\n");
+        printf("Enter you choice:");
+        scanf("%d",&choice);
+        if (choice==1)
+            {
+                printf("In which company?\n");
+                scanf("%s",company_name);
+                printf("\nwhat is your salary?\n");
+                scanf("%d",&salary);
+                printf("\n do you want to continue with your company?\n");
+                scanf("%c",&y_n);
+            }
+        if (choice==2)
+            {
+                printf("In which company?\n");
+                scanf("%s",company_name);
+                printf("what is your salary?\n");
+                scanf("%d",&salary);
+                printf("do you want to continue with your company?\n");
+                scanf("%c",&y_n);
+            }
+        if (choice==3)
+            {
+                printf("In which School?\n");
+                scanf("%s",school_name);
+                printf("what is your salary?\n");
 
-printf(  "1. engineer \n 2. doctor \n 3. teacher \n 4. youtuber \n 5. other\n");
-printf("Enter you choice:");
-scanf("%d",&choice);
-if (choice==1)
-        {printf("In which company?\n");
-        printf("what is your salary?\n");
-        printf("do you want to continue with your company?/n");
-        }
-if (choice==2)
-    {printf("In which company?\n");
-    printf("what is your salary?\n");
-    printf("do you want to continue with your company?\n");
+                printf("do you want to continue with your School?\n");
+            }
     }
-if (choice==3)
-    {printf("In which School?\n");
-    printf("what is your salary?\n");
-    printf("do you want to continue with your School?\n");
-    }
-getch();
+
+
 }
 /*
 hello
