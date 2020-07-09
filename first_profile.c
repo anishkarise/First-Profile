34e63a94872a042130937814ebbc1fce5f3c1e39
anishkarise <ranishk38@gmail.com>
2020-07-09 13:55:11 +0530

    making it type safe

--- first_profile.c
+++ first_profile.c
@@ -1,36 +1,85 @@
 #include<stdio.h>
+#include<stdlib.h>
 void main ()
-{int age,choice,salary,school;
-char company_name[20],y_n,school_name[30],hospital_name,youtube_channel_name,what_job_name;
-//scanf("%[^\n]%*c");
-printf("hello!");
-printf("tell me your age?\n");
-scanf("%d",&age);
-printf("your age is %d\n",age);
-printf("\n");
+{int age,choice,salary;
+ char company_name[20],y_n,school_name[30],hospital_name[30],youtube_channel_name[10],job_name[10],your_name[10];
+ char buffer[1024];
+//scanf("%[^\n]%*c",);
+//printf("hello!");
+//snprintf(buffer,6,"%s","Hello!");
+
+sprintf(buffer,"Hello\n");
+printf("%s",buffer);
+
+//fprintf(stdin,"Hello");
+
+//printf("what is your name?\n");
+sprintf(buffer,"What is your name?\n");
+printf("%s",buffer);
+
+fgets(your_name,sizeof(your_name),stdin);
+sscanf(your_name,"%s",your_name);
+
+//scanf("%[^\n]%*c",your_name);
+
+sprintf(buffer,"Hello,%s",your_name);
+printf("%s",buffer);
+
+
+//printf("\ntell me your age?\n");
+sprintf(buffer,"\nTell me your age?\n");
+printf("%s",buffer);
+
+
+fgets(buffer,3,stdin);
+sscanf(buffer,"%d",&age);
+//age=atoi(buffer);
+//scanf("%d",&age);
+
+
+//printf("your age is %d",age);
+sprintf(buffer,"Your age is %d\n",age);
+printf("%s",buffer);
 if(age<21)
     {
-        printf("you are kid can't go ahead\n");
-        getch();
+        sprintf(buffer,"you are kid can't go ahead\n");
+        printf("%s",buffer);
+
     }
 else
-    {
+    {// Rest type safing
         printf("you are big kid you can continue\n\n");
         printf("what work do you do?\n");
-        printf("1. engineer \n 2. doctor \n 3. teacher \n 4. youtuber \n 5. other\n");
+        printf(" 1.engineer \n 2. doctor \n 3. teacher \n 4. youtuber \n 5. other\n");
         printf("Enter you choice:");
-        scanf("%d",&choice);
+        //memset(buffer,'\0',sizeof(buffer));
+        //buffer[0]='\0';
+        //fgets(buffer,3,stdin);
+        fgets(buffer,3,stdin);
+        sscanf(buffer,"%d",&choice);
+        //printf("%s",buffer);
+        //choice=atoi(buffer);
+        //scanf("%d",&choice);
+        //fflush(stdin); // don't use
+        //scanf("%[^\n]%*c",company_name);
+        //choice=1;
         if (choice==1)
             {
                 printf("In which company?\n");
-                scanf("%s",company_name);
+                //printf("Debug , this is a debug message");
+                //scanf("%s",&company_name);
+                //printf("hello");
+                //scanf("%*[^\n]%*c",company_name);
+                fgets(company_name,sizeof(company_name),stdin);
                 printf("\nwhat is your salary?\n");
                 scanf("%d",&salary);
                 printf("\n do you want to continue with your company?\n");
                 scanf("%c",&y_n);
+
             }
         if (choice==2)
             {
+                printf("Debug, choice 2");
                 printf("In which company?\n");
                 scanf("%s",company_name);
                 printf("what is your salary?\n");
@@ -48,7 +97,7 @@ else
             }
     }
 
-
+getch();
 }
 /*
 hello
@@ -74,5 +123,17 @@ If(choice==3)
 
 */
 
-
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int age2;
+  char name[20];
+  //char age[3];
+ printf("%d",atoi(fgets(name,sizeof(name),stdin)));
+printf("%s",name);
+    return 0;
+}
+*/
 
