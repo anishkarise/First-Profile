9eb8d84842216d21c891cf14626f576bd4ddbc02
anishkarise <ranishk38@gmail.com>
2020-07-12 13:21:24 +0530

    added file export

--- first_profile.c
+++ first_profile.c
@@ -1,14 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+
+void write_file(char data[])
+{
+    //char *murgi="murgi anda";
+    FILE *file;
+    file=fopen("first_profile.txt","a");
+    fputs(data,file);
+    fputs("\n",file);
+    fclose(file);
+
+}
+
+
+
+
 void main ()
 {int age=0,choice=0,salary=0,gender=0;
  char company_name[20],y_n,school_name[30],hospital_name[30],youtube_channel_name[20],job_name[20],your_name[20];
  char buffer[1024];
-
-
+FILE *file;
+file=fopen("first_profile.txt","w");
+fclose(file);
 
 sprintf(buffer,"Hello\n");
 printf("%s",buffer);
+
 
 
 sprintf(buffer,"What is your name?\n");
@@ -17,6 +35,7 @@ printf("%s",buffer);
 fgets(your_name,sizeof(your_name),stdin);
 sscanf(your_name,"%s",your_name);
 
+write_file(your_name);
 
 sprintf(buffer,"Hello,%s",your_name);
 printf("%s",buffer);
@@ -28,6 +47,8 @@ printf("%s",buffer);
 
 fgets(buffer,sizeof(buffer),stdin);
 sscanf(buffer,"%d",&age);
+
+write_file(buffer);
 
 sprintf(buffer,"Your age is %d\n",age);
 printf("%s",buffer);
@@ -50,19 +71,25 @@ else
         fgets(buffer,3,stdin);
         sscanf(buffer,"%d",&choice);
 
+
         if (choice==1)
             {
+                write_file("Engineer");
                 sprintf(buffer,"In which company?\n");
                 printf("%s",buffer);
 
                 fgets(company_name,sizeof(company_name),stdin);
                 sscanf(company_name,"%s",company_name);
 
+                write_file(company_name);
+
                 sprintf(buffer,"\nwhat is your salary?\n");
                 printf("%s",buffer);
 
                 fgets(buffer,sizeof(buffer),stdin);
                 sscanf(buffer,"%d",&salary);
+
+                write_file(buffer);
 
                 sprintf(buffer,"\nDo you want to continue with your company?\n");
                 printf("%s",buffer);
@@ -73,20 +100,27 @@ else
                 sprintf(buffer,"Your choice is %c",y_n);
                 printf("%s",buffer);
 
+                write_file(buffer);
+
             }
         if (choice==2)
             {
+                write_file("doctor");
                 sprintf(buffer,"in which hospital?\n");
                 printf("%s",buffer);
 
                 fgets(hospital_name,sizeof(hospital_name),stdin);
                 sscanf(hospital_name,"%s",hospital_name);
 
+                write_file(hospital_name);
+
                 sprintf(buffer,"\nwhat is your salary?\n");
                 printf("%s",buffer);
 
                 fgets(buffer,sizeof(buffer),stdin);
                 sscanf(buffer,"%d",&salary);
+
+                write_file(buffer);
 
                 sprintf(buffer,"\nDo you want to continue with your company?\n");
                 printf("%s",buffer);
@@ -97,21 +131,28 @@ else
                 sprintf(buffer,"Your choice is %c",y_n);
                 printf("%s",buffer);
 
+                write_file(buffer);
+
 
             }
         if (choice==3)
             {
+                write_file("Teacher");
                 sprintf(buffer,"In which school?\n");
                 printf("%s",buffer);
 
                 fgets(school_name,sizeof(school_name),stdin);
                 sscanf(school_name,"%s",school_name);
 
+                write_file(school_name);
+
                 sprintf(buffer,"\nwhat is your salary?\n");
                 printf("%s",buffer);
 
                 fgets(buffer,sizeof(buffer),stdin);
                 sscanf(buffer,"%d",&salary);
+
+                write_file(buffer);
 
                 sprintf(buffer,"\nDo you want to continue with your school?\n");
                 printf("%s",buffer);
@@ -122,20 +163,27 @@ else
                 sprintf(buffer,"Your choice is %c",y_n);
                 printf("%s",buffer);
 
+                write_file(buffer);
+
             }
         if (choice==4)
             {
+                write_file("you tuber");
                 sprintf(buffer,"what is the name of your channel?\n");
                 printf("%s",buffer);
 
                 fgets(youtube_channel_name,sizeof(youtube_channel_name),stdin);
                 sscanf(youtube_channel_name,"%s",youtube_channel_name);
 
+                write_file(youtube_channel_name);
+
                 sprintf(buffer,"\nwhat is your income?\n");
                 printf("%s",buffer);
 
                 fgets(buffer,sizeof(salary),stdin);
                 sscanf(buffer,"%d",&salary);
+
+                write_file(buffer);
 
                 sprintf(buffer,"\nDo you want to continue with your youtube career?\n");
                 printf("%s",buffer);
@@ -146,11 +194,14 @@ else
                 sprintf(buffer,"Your choice is %c",y_n);
                 printf("%s",buffer);
 
+                write_file(buffer);
+
 
             }
 
         if ( choice == 5)
             {
+                write_file("other");
                 //printf("Debug, checking ");
                 sprintf(buffer,"\n1.men \n2.women");
                 printf("%s",buffer);
@@ -161,11 +212,14 @@ else
 
                 if (choice == 1)
                    {
+                       write_file("men");
                         sprintf(buffer,"job name?\n");
                         printf("%s",buffer);
 
                         fgets(job_name,sizeof(job_name),stdin);
                         sscanf(job_name,"%s",job_name);
+
+                        write_file(job_name);
 
                         sprintf(buffer,"name of the company?\n");
                         printf("%s",buffer);
@@ -173,11 +227,15 @@ else
                         fgets(company_name,sizeof(company_name),stdin);
                         sscanf(company_name,"%s",company_name);
 
+                        write_file(company_name);
+
                         sprintf(buffer,"What is your salary?\n");
                         printf("%s",buffer);
 
                         fgets(buffer,sizeof(buffer),stdin);
                         sscanf(buffer,"%d",&salary);
+
+                        write_file(buffer);
 
                         sprintf(buffer,"\nDo you want to continue with your company?\n");
                         printf("%s",buffer);
@@ -188,10 +246,13 @@ else
                         sprintf(buffer,"Your choice is %c",y_n);
                         printf("%s",buffer);
 
+                        write_file(buffer);
+
                    }
 
                         if (choice==2)
                            {
+                                write_file("women");
                                 sprintf(buffer,"\n1.house wife \n2.working woman");
                                 printf("%s",buffer);
                                 sprintf(buffer,"\nEnter you choice\n");
@@ -201,25 +262,31 @@ else
 
                                 if (choice==1)
                                 {
+                                    write_file("house wife");
                                     sprintf(buffer,"you are a house wife,sorry can't continue\n");
                                     printf("%s",buffer);
                                 }
 
                                 if (choice==2)
                                 {
-
-
+                                    write_file("working woman");
                                     sprintf(buffer,"what is the name of company?\n");
                                     printf("%s",buffer);
 
+                                    write_file(job_name);
+
                                     fgets(company_name,sizeof(company_name),stdin);
                                     sscanf(company_name,"%s",company_name);
+
+                                    write_file(company_name);
 
                                     sprintf(buffer,"What is your salary?\n");
                                     printf("%s",buffer);
 
                                     fgets(buffer,sizeof(buffer),stdin);
                                     sscanf(buffer,"%d",&salary);
+
+                                    write_file(buffer);
 
                                     sprintf(buffer,"\nDo you want to continue with your company?\n");
                                     printf("%s",buffer);
@@ -229,6 +296,8 @@ else
 
                                     sprintf(buffer,"Your choice is %c",y_n);
                                     printf("%s",buffer);
+
+                                    write_file(buffer);
                                 }
 
 
